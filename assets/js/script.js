d227106d1592fccaafc76f874a85a44841356076
halbeisent <dev@halbeisen.fr>
2018-03-14 15:21:49 +0100

    Edition de la navbar et ajout de la version anglaise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
-/*navbar related*/
+/*Effet scroll au click pour chaque section du CV*/
+$(function() {
+  var scrollToAnchor = function( id ) {
+    var elem = $("section[id='"+ id +"']"); // on crée une balise d'ancrage
+    if ( typeof elem.offset()  === "undefined" ) { // on verifie si l'élément existe
+		elem = $("#"+id); }
+    if ( typeof elem.offset()  !== "undefined" ) { // si l'élément existe, on continue
+      $('html, body').animate({
+              scrollTop: elem.offset().top }, 1500 );} // on défini un temps de défilement de page
+  };
+  $("a").click(function( event ) { // on attache la fonction au click
+    if ( $(this).attr("href").match("#") ) { // on vérifie qu'il s'agit d'une ancre
+      event.preventDefault();
+      var href = $(this).attr('href').replace('#', '') // on scroll vers la cible
+      scrollToAnchor( href ); }
+  });
+});
+
+/*Initialisation de wow.min.js*/
+new WOW().init();
 
 /*Code that displays the colored bar in the skillbars*/
-
 $(".bar").each(function(){
   $(this).find(".bar-inner").animate({
     width: $(this).attr("data-width")
