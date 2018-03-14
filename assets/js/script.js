58b5959f634ec7cba0b4fc7b360cf748e8705da8
halbeisent <dev@halbeisen.fr>
2018-03-14 11:40:19 +0100

    Navbar bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,4 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-}
+/*navbar related*/
 
 /*Code that displays the colored bar in the skillbars*/
 
